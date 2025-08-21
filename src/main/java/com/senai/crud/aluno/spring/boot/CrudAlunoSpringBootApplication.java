9bf5566c24e0f90d897bda957a1f61d691882922
Gabrieli da Silva Marcelino <gabih.12161@gmail.com>
2025-08-21 08:50:00 -0300

    Meu primeiro projeto Spring boot - 21/08/2025

---
+++ src/main/java/com/senai/crud/aluno/spring/boot/CrudAlunoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.senai.crud.aluno.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAlunoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAlunoSpringBootApplication.class, args);
+	}
+
+}
