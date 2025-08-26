e0add2dacb0d02e9b4511e3d24e2d784067ef5a2
Gabrieli da Silva Marcelino <gabih.12161@gmail.com>
2025-08-26 09:22:37 -0300

    Atualização do código do spring boot - 26/08/2025

--- src/main/java/com/senai/crud/aluno/spring/boot/Model/Aluno.java
+++ src/main/java/com/senai/crud/aluno/spring/boot/Model/Aluno.java
@@ -1,75 +1,37 @@
 package com.senai.crud.aluno.spring.boot.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Aluno {
-    @id
+    @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     protected String id;
+
+    @NotBlank(message = "O nome do aluno não pode estar vazio")
+    @Column(nullable = false, length = 100)
     protected String nome;
-    protected int cpf;
+
+    @NotNull(message = "O CPF não pode estar vazio")
+    @Column(nullable = false, length = 11)
+    protected String cpf;
+
     protected int idAcesso;
+
+    @NotBlank(message = "O E-mail não pode estar vazio")
+    @Column(nullable = false, length = 100)
     protected String email;
+
     protected String curso;
 
-    public Aluno(String id, String nome, int cpf, int idAcesso, String email, String curso) {
-        this.id = id;
-        this.nome = nome;
-        this.cpf = cpf;
-        this.idAcesso = idAcesso;
-        this.email = email;
-        this.curso = curso;
-    }
-
-    public  String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(int cpf) {
-        this.cpf = cpf;
-    }
-
-    public int getIdAcesso() {
-        return idAcesso;
-    }
-
-    public void setIdAcesso(int idAcesso) {
-        this.idAcesso = idAcesso;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCurso() {
-        return curso;
-    }
-
-    public void setCurso(String curso) {
-        this.curso = curso;
-    }
 }
-
