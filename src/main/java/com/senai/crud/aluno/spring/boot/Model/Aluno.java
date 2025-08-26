8c42fa298282fcaaaa7d8943f5b2c7f6e3d5f8c9
Gabrieli da Silva Marcelino <gabih.12161@gmail.com>
2025-08-26 09:24:03 -0300

    Atualização do código do spring boot - 26/08/2025

--- src/main/java/com/senai/crud/aluno/spring/boot/Model/Aluno.java
+++ src/main/java/com/senai/crud/aluno/spring/boot/Model/Aluno.java
@@ -3,15 +3,13 @@ package com.senai.crud.aluno.spring.boot.Model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+@Data
 @Entity
 public class Aluno {
     @Id
